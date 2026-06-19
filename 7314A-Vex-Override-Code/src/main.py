a54a2fa12e1cdd494a361b9ff5f2cf79234f0339
Ibrahim Helal <contact.ihelal@gmail.com>
2026-06-18 19:32:39 -0700

    Add Mecanum Drive Code

--- 7314A-Vex-Override-Code/src/main.py
+++ 7314A-Vex-Override-Code/src/main.py
@@ -1,35 +1,44 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
-# 	Author:       student                                                      #
-# 	Created:      6/18/2026, 6:42:57 PM                                        #
+# 	Author:       ibrahimhelal                                                 #
+# 	Created:      4/23/2026, 6:07:40 PM                                        #
 # 	Description:  V5 project                                                   #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
 # Library imports
 from vex import *
+from vex import Motor, FORWARD, PERCENT, Ports
+
+class MecanumDrive:
+    def __init__(self, fl_motor, fr_motor, bl_motor, br_motor):
+        self.fl_motor : Motor = fl_motor
+        self.fr_motor : Motor = fr_motor
+        self.bl_motor : Motor = bl_motor
+        self.br_motor : Motor = br_motor
+        
+    def drive(self, fwd, strafe, turn):        
+        fl = fwd + strafe + turn
+        fr = fwd - strafe - turn
+        bl = fwd - strafe + turn
+        br = fwd + strafe - turn
+        
+        top_speed = max(abs(fl), abs(fr), abs(bl), abs(br), 100)
+        scale = 100.0 / top_speed
+        
+        self.fl_motor.spin(FORWARD, -(fl * scale), PERCENT)
+        self.fr_motor.spin(FORWARD, (fr * scale), PERCENT)
+        self.bl_motor.spin(FORWARD, -(bl * scale), PERCENT)
+        self.br_motor.spin(FORWARD, (br * scale), PERCENT)
+
+fl_motor = Motor(Ports.PORT20)
+fr_motor = Motor(Ports.PORT12)
+br_motor = Motor(Ports.PORT1)  
+bl_motor = Motor(Ports.PORT3)
+
+my_robot = MecanumDrive(fl_motor, fr_motor, bl_motor, br_motor)
 
 brain = Brain()
 
-# Jackson's changes
-
-#hash brown
-
-def autonomous():
-    brain.screen.clear_screen()
-    brain.screen.print("autonomous code")
-    # place automonous code here
-
-def user_control():
-    brain.screen.clear_screen()
-    brain.screen.print("driver control")
-    # place driver control in this while loop
-    while True:
-        wait(20, MSEC)
-
-# create competition instance
-comp = Competition(user_control, autonomous)
-
-# actions to do when the program starts
-brain.screen.clear_screen()
+my_robot.drive(50, 50, 50)  # Drive forward at 50% speed
