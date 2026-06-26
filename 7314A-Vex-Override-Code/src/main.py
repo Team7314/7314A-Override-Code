8a429284aa765ecc15acc5be1e80041dbc6933ed
Team 7314 <7314@rollingrobots.com>
2026-06-25 19:51:23 -0700

    Added color sensor

--- 7314A-Vex-Override-Code/src/main.py
+++ 7314A-Vex-Override-Code/src/main.py
@@ -11,6 +11,7 @@
 from vex import *
 from vex import Motor, FORWARD, PERCENT, Ports
 
+
 class MecanumDrive:
     def __init__(self, fl_motor, fr_motor, bl_motor, br_motor):
         self.fl_motor : Motor = fl_motor
@@ -32,36 +33,65 @@ class MecanumDrive:
         self.bl_motor.spin(FORWARD, -(bl * scale), PERCENT)
         self.br_motor.spin(FORWARD, (br * scale), PERCENT)
 
-fl_motor = Motor(Ports.PORT1)
-fr_motor = Motor(Ports.PORT10)
-br_motor = Motor(Ports.PORT20)  
-bl_motor = Motor(Ports.PORT11)
 
-my_robot = MecanumDrive(fl_motor, fr_motor, bl_motor, br_motor)
+class Robot:
+    def __init__(self):
+        self.brain = Brain()
+        fl_motor = Motor(Ports.PORT1)
+        fr_motor = Motor(Ports.PORT10)
+        br_motor = Motor(Ports.PORT20)  
+        bl_motor = Motor(Ports.PORT11)
+        self.pin_color_sensor = Optical(Ports.PORT15)
+        self.drive_base = MecanumDrive(fl_motor, fr_motor, bl_motor, br_motor)
+        self.pin_color_sensor.set_light_power(100, PERCENT)
+        self.controller = Controller()
+    
+    def update_display_color(self):
+        color = self.pin_color_sensor.color()
+        if color == Color.RED:
+            self.brain.screen.set_fill_color(Color.RED)
+            self.brain.screen.draw_rectangle(0, 0, 480, 240)
+        elif color == Color.YELLOW:
+            self.brain.screen.set_fill_color(Color.YELLOW)
+            self.brain.screen.draw_rectangle(0, 0, 480, 240)
+        elif color == Color.BLUE:
+            self.brain.screen.set_fill_color(Color.BLUE)
+            self.brain.screen.draw_rectangle(0, 0, 480, 240)
+        else:
+            self.brain.screen.set_fill_color(Color.BLACK)
+            self.brain.screen.draw_rectangle(0, 0, 480, 240)
 
-brain = Brain()
+    def autonomous(self):
+        self.brain.screen.clear_screen()
+        self.brain.screen.print("autonomous code")
+        # place automonous code here
+    
+    def user_control(self):
+        self.brain.screen.clear_screen()
+        self.brain.screen.print("driver control")
+        # place driver control in this while loop
 
-controller = Controller()
+
+        while True:
+            fwd = self.controller.axis3.position()
+            strafe = self.controller.axis4.position()
+            turn = self.controller.axis1.position()
+            self.drive_base.drive(fwd, strafe, turn)
+
+            self.update_display_color()
+            
+            wait(1, MSEC)
+
+robot = Robot()
 
 def autonomous():
-    brain.screen.clear_screen()
-    brain.screen.print("autonomous code")
-    # place automonous code here
+    robot.autonomous()
 
 def user_control():
-    brain.screen.clear_screen()
-    brain.screen.print("driver control")
-    # place driver control in this while loop
-    while True:
-        fwd = controller.axis3.position()
-        strafe = controller.axis4.position()
-        turn = controller.axis1.position()
-        my_robot.drive(fwd, strafe, turn)
-        
-        wait(1, MSEC)
+    robot.user_control()
 
 # create competition instance
 comp = Competition(user_control, autonomous)
 
 # actions to do when the program starts
-brain.screen.clear_screen()
+robot.brain.screen.clear_screen()
